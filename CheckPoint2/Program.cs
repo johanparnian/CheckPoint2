50d1c548b4ee27b8d2ce5701c570a7b38d5d3006
Johan P <Johan@DESKTOP-M4MSCGK>
2022-07-04 08:49:00 +0200

    Add project files.

---
+++ CheckPoint2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CheckPoint2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
