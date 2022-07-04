c86a467bebc82247e6a9111e49f61bd895222a46
Johan P <Johan@DESKTOP-M4MSCGK>
2022-07-04 09:58:38 +0200

    Added Unittest for creating instance of Employee.

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -14,11 +14,11 @@ namespace TestProject1
             bool check = false;
             Company C1 = new("aw", 50);
 
-            if (C1.Name == "aw" && C1.EmployeeCount==50)
+            if (C1.Name == "aw" && C1.EmployeeCount == 50)
             {
                 check = true;
             }
-            
+
             Assert.IsTrue(check);
         }
 
@@ -38,6 +38,25 @@ namespace TestProject1
             Assert.IsTrue(check);
         }
 
+        [Test]
+        public void TestEmployeeInstance()
+        {
+            bool check = false;
+            string name = "Johan";
+            int age = 34;
+            DateTime hireDate = DateTime.Now; 
+            Employee E1 = new(name, age, hireDate);
+
+
+            if (E1.Name == name && E1.Age == age && hireDate == E1.HireDate)  
+            {
+                check = true;
+            }
+
+            Assert.IsTrue(check);
+            // Her kan DateTime.Now i actual og expected være bittelitt ulik hvis testen utføres litt tregt (tror jeg).
+            // Så jeg burde heller ha satt faste tider.
+        }
 
     }
 }
