1a08e0a933b9c189f899cff6ecd0ae2949235d88
Johan P <Johan@DESKTOP-M4MSCGK>
2022-07-04 10:12:43 +0200

    Created "independent Contractor" Class inheritance from "Person".

---
+++ CheckPoint2/IndependentContractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint2
+{
+    public class IndependentContractor : Person
+    {
+        public DateTime HireDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public int ContractValue { get; private set; }
+
+
+        public IndependentContractor(string name, int age, DateTime hireDate, DateTime endDate) : base(name, age)
+        {
+            HireDate = hireDate;
+            EndDate = endDate;
+        }
+
+
+    }
+}
--- CheckPoint2/Person.cs
+++ CheckPoint2/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CheckPoint2
 {
-    public class Person
+    public class Person //Denne ville jeg gjort abstract om ikke det var for Unit-testingen. 
     {
         public string Name { get; private set; }
         public int Age { get; set; }
